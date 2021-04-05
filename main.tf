b04397bc20a9e34c9954620260eff5281cd5e056
Michael Tharpe <mtharpe@hashicorp.com>
2021-04-05 11:08:05 -0400

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,140 @@
+#########################################
+# Specify the provider and access details
+#########################################
+provider "aws" {
+  region  = var.aws_region
+  version = ">= 2.0"
+}
+
+###########################################
+# Create a VPC to launch our instances into
+###########################################
+resource "aws_vpc" "demo-tfe" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "demo-tfe"
+  }
+}
+
+###########################################################################
+# Create an internet gateway to give our subnet access to the outside world
+###########################################################################
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.demo-tfe.id
+}
+
+#######################################################
+# Grant the VPC internet access on its main route table
+#######################################################
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.demo-tfe.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+##############################
+# Setup key for authentication
+##############################
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+##################
+# Setup Web Subnet
+##################
+resource "aws_subnet" "web" {
+  vpc_id                  = aws_vpc.demo-tfe.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Web"
+  }
+}
+
+########################################
+# List all availability zones to be used
+########################################
+data "aws_availability_zones" "available" {
+}
+
+#########################
+# Setup Management Subnet
+#########################
+resource "aws_subnet" "management" {
+  vpc_id                  = aws_vpc.demo-tfe.id
+  cidr_block              = "10.0.100.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Management"
+  }
+}
+
+########################
+# Default Security group
+########################
+resource "aws_security_group" "default" {
+  name        = "demo-tfe default sg"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.demo-tfe.id
+
+  # SSH access from the VPC and LocalIP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16", "0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##############
+# Web Instance
+##############
+resource "aws_instance" "web-01" {
+  tags = {
+    Name = "web-01"
+  }
+
+  connection {
+    host = coalesce(self.public_ip, self.private_ip)
+    type = "ssh"
+    user = "ubuntu"
+    private_key = var.private_key
+  }
+
+  instance_type = "t2.micro"
+
+  ami = var.aws_amis
+  key_name = aws_key_pair.auth.id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  subnet_id = aws_subnet.web.id
+
+  provisioner "chef" {
+    environment     = var.chef_environment
+    run_list        = [var.server_runlist]
+    node_name       = aws_instance.web-01.tags.Name
+    server_url      = var.chef_server_url
+    recreate_client = true
+    version         = "13.12.3"
+    user_name       = var.chef_username
+    user_key        = var.chef_pem
+  }
+}
+
+resource "aws_route53_record" "web-01" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "web-01.axis.local"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.web-01.private_ip]
+}
---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "public_key" {
+  description = "Public key info"
+}
+
+variable "private_key" {
+  description = "Private key info"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default = "terraform"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers"
+  default = "us-east-2"
+}
+
+variable "aws_amis" {
+  description = "AWS AMI ID used for machine deployments"
+  default = ""
+}
+
+# Chef global vars
+variable "chef_environment" {
+  description = "Chef Environment the machine will join"
+  default = "_default"
+}
+
+variable "server_runlist" {
+  description = "Chef Runlist items"
+  default = "chef-linux-base-recipe::default"
+}
+
+variable "chef_server_url" {
+  description = "Chef Server URL"
+}
+
+variable "chef_username" {
+  description = "Chef Username that will be used to auth to Chef"
+}
+
+variable "chef_pem" {
+  description = "Chef PEM that is the match to the above defined Chef username"
+}
