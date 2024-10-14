bbc6841da85d4f5927e32179e94a5d25b45cc27b
Michael Tharpe <mtharpe@gmail.com>
2024-10-14 08:42:34 -0400

    Updated for new syntax

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 #########################################
 # Specify the provider and access details
 #########################################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = var.aws_region
-  version = ">= 2.0"
+  region = var.aws_region
 }
 
 ###########################################
